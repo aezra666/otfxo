27d94a52fb026efc8251e2a01784999aeb42f150
aezra666 <150811192+aezra666@users.noreply.github.com>
2026-09-08 15:25:28 +0300

    Update script.js
    Changing time

--- script.js
+++ script.js
@@ -157,7 +157,7 @@ $ client info
 $ enter otfxo_world
 > welcome`;
 
-    typeWriter(introTypewriter, '𝗢𝗧𝗙𝗫𝗢', 50, () => {
+    typeWriter(introTypewriter, '𝗢𝗧𝗙𝗫𝗢', 80, () => {
       proceedBtn.style.display = 'block';
       proceedBtn.disabled = false;
     });
