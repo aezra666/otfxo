eaed5f5093e60c112510d21e8e1fe1c609395edb
aezra666 <150811192+aezra666@users.noreply.github.com>
2026-09-14 16:18:57 +0300

    Update profile-config.js

--- profile-config.js
+++ profile-config.js
@@ -1,13 +1,13 @@
 window.OTFXO_PROFILES = {
   '1034804876733071382': {//aezra
     banner: 'https://file.garden/ap_Ebnzi9V7bLTDs/Tiny%20Stories_%20They%20Come%20At%20Night.gif',
-    music: "https://file.garden/ap_Ebnzi9V7bLTDs/aezra.mp3"
+    music: "https://file.garden/ap_Ebnzi9V7bLTDs/Escape%20-%20Nemzzz%20(Remix%20by%20AJ)%20-%20AJ%20OTB%20(128k).mp3"
   },
   '1483321828838477966': {//jay
     banner: 'https://file.garden/ap_Ebnzi9V7bLTDs/download.gif',
     music: 'https://file.garden/ap_Ebnzi9V7bLTDs/Janice%20STFU.mp3'
   },
-  '855390015940984853': {
+  '855390015940984853': {//chano
     banner: 'https://file.garden/ap_Ebnzi9V7bLTDs/d7ff7069ff4939400d34415d1de21543.gif',
     music: 'https://file.garden/ap_Ebnzi9V7bLTDs/Chris%20Brown%20-%20Loyal%20(Lyrics)%20ft.%20Lil%20Wayne%2C%20Tyga.mp3'
   },
